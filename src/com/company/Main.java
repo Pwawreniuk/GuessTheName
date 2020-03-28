b3da2049c448d240ee58f27e6b244e0969a77d14
PiotrWawreniuk <piotr21wawreniuk@gmail.com>
2020-03-28 10:22:04 +0100

    Initialazing project

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        File file = new File("Movies.txt");
+        Scanner fileScanner = new Scanner(file);
+
+        while (fileScanner.hasNextLine()) {
+            String line = fileScanner.nextLine();
+
+        }
+    }
+}
