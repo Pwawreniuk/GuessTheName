9bcec8f56a38dd0343834e4482ad731636861575
PiotrWawreniuk <piotr21wawreniuk@gmail.com>
2020-03-28 10:55:44 +0100

    choosing random movie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        File file = new File("Movies.txt");
-        Scanner fileScanner = new Scanner(file);
+        String choosenMovie = null;
 
-        while (fileScanner.hasNextLine()) {
-            String line = fileScanner.nextLine();
-            System.out.println(line);
+        File file = new File("Movies.txt");
+        Scanner fileScanner1 = new Scanner(file);
+
+        int movieNum = 0;
+        while (fileScanner1.hasNextLine()) {
+            String line = fileScanner1.nextLine();
+            movieNum++;
         }
+
+        int randMovie = (int) (Math.random() * movieNum) + 1;
+        Scanner fileScanner2 = new Scanner(file);
+        for(int i = 0; i < randMovie; i++) {
+            choosenMovie = fileScanner2.nextLine();
+        }
+        System.out.println(choosenMovie);
     }
 }
